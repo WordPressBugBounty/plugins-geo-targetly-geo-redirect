7a6107cd0f16ab57930c89ae685c44eb9c8d9c49
Sean Murphy <sean@iamseanmurphy.com>
2025-06-30 06:20:12 +0000

    7.1

--- geo-targetly-geo-redirect/geotargetly-geo-redirect.php
+++ geo-targetly-geo-redirect/geotargetly-geo-redirect.php
@@ -2,7 +2,7 @@
 /*
 * Plugin Name: Geo Redirect
 * Description: Redirect your website by geo location
-* Version: 7.0
+* Version: 7.1
 * Author: Geo Targetly
 * Author URI: https://geotargetly.com
 */
@@ -16,49 +16,49 @@ add_action( 'admin_menu', 'geotargetly_wp_georedirect_add_admin_menu' );
 add_action( 'admin_init', 'geotargetly_wp_georedirect_settings_init' );
 
 
-function geotargetly_wp_georedirect_add_admin_menu(  ) { 
+function geotargetly_wp_georedirect_add_admin_menu(  ) {
 	add_menu_page( 'Geo Redirect - Geo Targetly', 'Geo Redirect', 'manage_options', 'geotargetly_wp_georedirect', 'geotargetly_wp_georedirect_options_page', 'dashicons-location' );
 }
 
 
-function geotargetly_wp_georedirect_settings_init(  ) { 
+function geotargetly_wp_georedirect_settings_init(  ) {
 	register_setting( 'geotargetly_wp_georedirect_pluginPage', 'geotargetly_wp_georedirect_settings' );
 	add_settings_section(
-		'geotargetly_wp_georedirect_pluginPage_section', 
-		__( '', 'geotargetly_wp_georedirect' ), 
-		'geotargetly_wp_georedirect_settings_section_callback', 
+		'geotargetly_wp_georedirect_pluginPage_section',
+		__( '', 'geotargetly_wp_georedirect' ),
+		'geotargetly_wp_georedirect_settings_section_callback',
 		'geotargetly_wp_georedirect_pluginPage'
 	);
-     add_settings_field( 
-		'geotargetly_wp_georedirect_ids', 
-		__( 'IDs (comma separated)', 'geotargetly_wp_georedirect' ), 
-		'geotargetly_wp_georedirect_ids_render', 
-		'geotargetly_wp_georedirect_pluginPage', 
-		'geotargetly_wp_georedirect_pluginPage_section' 
+     add_settings_field(
+		'geotargetly_wp_georedirect_ids',
+		__( 'IDs (comma separated)', 'geotargetly_wp_georedirect' ),
+		'geotargetly_wp_georedirect_ids_render',
+		'geotargetly_wp_georedirect_pluginPage',
+		'geotargetly_wp_georedirect_pluginPage_section'
 	);
 }
 
 
 
-function geotargetly_wp_georedirect_ids_render(  ) { 
+function geotargetly_wp_georedirect_ids_render(  ) {
 	$options = get_option( 'geotargetly_wp_georedirect_settings' );
 	?>
-	<input type='text' name='geotargetly_wp_georedirect_settings[geotargetly_wp_georedirect_ids]' value='<?php echo $options['geotargetly_wp_georedirect_ids']; ?>'>
+	<input type='text' name='geotargetly_wp_georedirect_settings[geotargetly_wp_georedirect_ids]' value='<?php echo is_bool($options) ? '': $options['geotargetly_wp_georedirect_ids']; ?>'>
 	<?php
 }
 
 
 
-function geotargetly_wp_georedirect_settings_section_callback(  ) { 
+function geotargetly_wp_georedirect_settings_section_callback(  ) {
 	echo __( '', 'geotargetly_wp_georedirect' );
 }
 
 
-function geotargetly_wp_georedirect_options_page(  ) { 
+function geotargetly_wp_georedirect_options_page(  ) {
 
 	?>
 	<form action='options.php' method='post'>
-		
+
 <div id="post-body" class="metabox-holder columns-2">
 <div id="post-body-content">
 
@@ -71,7 +71,7 @@ function geotargetly_wp_georedirect_options_page(  ) {
 
 <p><strong>Note: </strong>This Wordpress plugin allows you to install Geo Targetly's Geo Redirect scripts into your Wordpress website.</p>
 
-<p style="padding-top:20px;"><strong>Steps</strong></p>	
+<p style="padding-top:20px;"><strong>Steps</strong></p>
 <p>1. Create a Geo Targetly account at <a href="https://geotargetly.com" target="_blank">Geotargetly.com</a> </p>
 <p>2. <a href="https://dashboard.geotargetly.com/login" target="_blank">Log in</a> to your Geotargetly dasboard</p>
 <p>3. Create a new <strong>Geo Redirect</strong> service</p>
@@ -114,19 +114,19 @@ submit_button();
 add_action('wp_head', 'geotargetly_wp_georedirect', -1000);
 
 function geotargetly_wp_georedirect() {
-	
+
 	$var = "some text";
 	$scripts = <<<EOT
 EOT;
-	
+
 	$geotargetly_georedirect_ids_database        = get_option('geotargetly_wp_georedirect_settings');
     $geotargetly_georedirect_ids_database_string = preg_replace('/\s+/', '', $geotargetly_georedirect_ids_database['geotargetly_wp_georedirect_ids']);
     $geotargetly_georedirect_ids_database_array  = explode(',', $geotargetly_georedirect_ids_database_string);
     $geotargetly_georedirect_ids_database_array  = array_filter($geotargetly_georedirect_ids_database_array);
-	
+
 	if (!empty($geotargetly_georedirect_ids_database_array)) {
         for ($i = 0; $i < count($geotargetly_georedirect_ids_database_array); ++$i) {
-            
+
 			$scripts .= <<<EOT
 <script>
 (function(g,e,o,id,t,a,r,ge,tl,y,s){
@@ -141,10 +141,10 @@ setTimeout(function(){s();}, 8000);
 })(document,'script','head');
 </script>
 EOT;
-            
+
         }
     }
-	
+
 	echo $scripts;
 }
 
--- geo-targetly-geo-redirect/readme.txt
+++ geo-targetly-geo-redirect/readme.txt
@@ -3,8 +3,8 @@ Contributors: Geo Targetly
 Donate link: https://geotargetly.com
 Tags: geo content, geo redirect, redirect by location, redirect by country, redirect by city, redirect by ip, ip redirect, geo ip redirect, ip to location, geo targeted redirect, geo location redirect, redirect website, url redirect, location based redirect
 Requires at least: 3.0.1
-Tested up to: 6.3
-Stable tag: 6.3
+Tested up to: 6.8
+Stable tag: 7.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
